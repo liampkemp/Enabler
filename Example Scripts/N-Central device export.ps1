ce1b29a6840f36046dcf4500a207ee9c8ef8cca0
Patrick Smits <patrick.smits@outlook.com>
2018-02-02 12:02:17 +0100

    Added 2 new functions + example scripts

---
+++ Example Scripts/N-Central device export.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module .\Enabler.psm1
+
+Connect-NCService -Url (Read-Host -Prompt "N-Central URL")
+
+Get-NCCustomerList | select parentid -Unique | foreach {Get-NCDeviceList -CustomerId $_.parentid | select customername,sitename,discoveredname,supportedoslabel} | export-csv -Path C:\temp\N-Central_devices.csv -NoClobber -NoTypeInformation
