2c7cf93205643d722d484e7ca1e637a306a29270
Patrick Smits <patrick.smits@outlook.com>
2018-02-27 10:08:31 +0100

    Fix in the example script

--- Example Scripts/N-Central asset export.ps1
+++ Example Scripts/N-Central asset export.ps1
@@ -3,25 +3,28 @@
 Connect-NCService -Url (Read-Host -Prompt "N-Central URL")
 
 
-$customerName = Read-Host "Customer name"
+$customerName = (Read-Host "Customer name").Split(',')
 
 $device = @()
-foreach($customer in (Get-NCCustomerList | where {$_.customername -eq $customerName}))
+foreach($c in $customerName)
 {
-    $device += Get-NCDeviceList -CustomerId $customer.customerid | select deviceid,longname,lastloggedinuser,deviceclass |`
-    ForEach-Object {
-        $assetData = Get-NCDeviceAssetInfoExport -DeviceId $_.deviceid -InformationCategoriesInclusion "asset.computersystem","asset.os"
-        $o = New-Object -TypeName psobject
-        $o | Add-Member NoteProperty clientName -Value $customer.customername
-        $o | Add-Member NoteProperty computerName -Value $_.longname
-        $o | Add-Member NoteProperty deviceClass -Value $_.deviceclass
-        $o | Add-Member NoteProperty lastLoggedInUser -Value $_.lastloggedinuser
-        $o | Add-Member NoteProperty manufacturer -Value $assetData.'asset.computersystem.manufacturer'
-        $o | Add-Member NoteProperty model -Value $assetData.'asset.computersystem.model'
-        $o | Add-Member NoteProperty serialNumber -Value $assetData.'asset.computersystem.serialnumber'
-        $o | Add-Member NoteProperty lastBootupTime -Value $assetData.'asset.os.lastbootuptime'
+    foreach($customer in (Get-NCCustomerList | where {$_.customername -eq $c}))
+    {
+        $device += Get-NCDeviceList -CustomerId $customer.customerid | select deviceid,longname,lastloggedinuser,deviceclass |`
+        ForEach-Object {
+            $assetData = Get-NCDeviceAssetInfoExport -DeviceId $_.deviceid -InformationCategoriesInclusion "asset.computersystem","asset.os"
+            $o = New-Object -TypeName psobject
+            $o | Add-Member NoteProperty clientName -Value $customer.customername
+            $o | Add-Member NoteProperty computerName -Value $_.longname
+            $o | Add-Member NoteProperty deviceClass -Value $_.deviceclass
+            $o | Add-Member NoteProperty lastLoggedInUser -Value $_.lastloggedinuser
+            $o | Add-Member NoteProperty manufacturer -Value $assetData.'asset.computersystem.manufacturer'
+            $o | Add-Member NoteProperty model -Value $assetData.'asset.computersystem.model'
+            $o | Add-Member NoteProperty serialNumber -Value $assetData.'asset.computersystem.serialnumber'
+            $o | Add-Member NoteProperty lastBootupTime -Value $assetData.'asset.os.lastbootuptime'
 
-        $o 
+            $o 
+        }
     }
 }
 
