0a8fc796a4642fe320f9f4c7c9415db067437902
Patrick Smits <patrick.smits@outlook.com>
2018-03-28 14:37:59 +0200

    changed the demo script a bit

--- Example Scripts/N-Central server offline alarm.ps1
+++ Example Scripts/N-Central server offline alarm.ps1
@@ -4,13 +4,22 @@ Connect-NCService -Url (Read-Host -Prompt "N-Central URL")
 
 $customers = Get-NCCustomerList
 
+clear
 while($true)
 {
-    Start-Sleep 30
     $agentsOffline = Get-NCActiveIssueList -Customer $customers -Acknowledged $false -Failed -SearchBy "Agent Status"
+    $agentsOffline += Get-NCActiveIssueList -Customer $customers -Acknowledged $false -Failed -SearchBy "Connectivity (VMware)"
 
     if($agentsOffline.Count -gt 0)
     {
+        clear
+        $agentsOffline | select 'activeissue.customername','activeissue.devicename','activeissue.deviceclass','activeissue.servicename','activeissue.transitiontime' -Unique | ft
         (New-Object Media.SoundPlayer "$pwd\Example Scripts\alarm.wav").PlaySync();
+    }
+    else
+    {
+        clear
+        Write-Host "No issues detected!" -ForegroundColor Green
+        Start-Sleep 30
     }
 }
